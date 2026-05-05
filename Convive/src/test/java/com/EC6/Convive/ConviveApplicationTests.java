d91c4f61c8ecc5a6b484770227719e724096cbf2
Diogo Santos Rodrigues <109999912+dioguit0s@users.noreply.github.com>
2026-05-05 19:46:50 -0300

    feat: genesis commit

---
+++ Convive/src/test/java/com/EC6/Convive/ConviveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EC6.Convive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConviveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
