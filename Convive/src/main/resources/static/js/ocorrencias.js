dfb57d07c54efa521206128944fb03b6d44c329a
Diogo <diogosantos152005@gmail.com>
2026-05-18 19:58:45 -0300

    fix: moderator commentary on ticket was not appearing for final user

--- Convive/src/main/resources/static/js/ocorrencias.js
+++ Convive/src/main/resources/static/js/ocorrencias.js
@@ -5,6 +5,7 @@ function abrirConsulta(elemento) {
     const prioridade = elemento.getAttribute('data-prioridade');
     const status = elemento.getAttribute('data-status').replace('_', ' ');
     const descricao = elemento.getAttribute('data-descricao');
+    const comentarioModerador = elemento.getAttribute('data-comentario');
 
     const campoProtocolo = document.getElementById('detalhe-protocolo');
     const campoData = document.getElementById('detalhe-data');
@@ -12,6 +13,8 @@ function abrirConsulta(elemento) {
     const campoStatus = document.getElementById('detalhe-status');
     const campoDescricao = document.getElementById('detalhe-descricao');
     const inputExcluir = document.getElementById('ocorrencia-id-excluir');
+    const campoComentarioModerador = document.getElementById('detalhe-comentario-moderador');
+    const containerComentario = document.getElementById('container-comentario-moderador');
 
     if (campoProtocolo) campoProtocolo.innerText = '#' + protocolo;
     if (campoData) campoData.innerText = data;
@@ -19,6 +22,16 @@ function abrirConsulta(elemento) {
     if (campoStatus) campoStatus.innerText = status;
     if (campoDescricao) campoDescricao.innerText = descricao;
     if (inputExcluir) inputExcluir.value = id;
+    if (campoComentarioModerador) campoComentarioModerador.innerText = comentarioModerador;
+
+if (comentarioModerador && comentarioModerador.trim() !== '') {
+        if (campoComentarioModerador) campoComentarioModerador.innerText = comentarioModerador;
+        if (containerComentario) containerComentario.classList.remove('hidden');
+    } else {
+        if (campoComentarioModerador) campoComentarioModerador.innerText = '';
+        if (containerComentario) containerComentario.classList.add('hidden');
+    }
+
 
     const modal = document.getElementById('modal-detalhes-ocorrencia');
     if (modal) {
